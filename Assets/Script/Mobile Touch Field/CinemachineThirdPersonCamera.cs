cd4e5723c45486735c13880ad02a3eca72e666ae
KRSBX <firdaus.sati50@gmail.com>
2020-12-29 13:31:35 +0700

    Third Person Camera with Cinemachine
    CinemachineThirdPersonCamera.cs added
    Cinemachine are needed to use this script

---
+++ Assets/Script/Mobile Touch Field/CinemachineThirdPersonCamera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CinemachineThirdPersonCamera : MonoBehaviour
+{
+    [SerializeField] CinemachineFreeLook freeLook;
+    [SerializeField] TouchFields touchPad;
+
+    private void Update() {
+        // Set the value in Cinemachine Free Look to the TouchDistance value in TouchFields.cs
+        freeLook.m_XAxis.m_InputAxisValue = touchPad.TouchDistance.x;
+        freeLook.m_YAxis.m_InputAxisValue = touchPad.TouchDistance.y;
+    }
+}
